9b6d72daba2b37a6da8055f067d2b708fb2981c1
Bekk Blando <bekkblando@gmail.com>
2016-05-14 23:46:06 -0400

    Almost done

--- RFID.py
+++ RFID.py
@@ -14,7 +14,7 @@ class Application(Frame):
 
     def bike(self, bike):
         print("This is Bike", bike)
-        self.checkout = Button(self, text = "Checkout", command = lambda: self.checkoutbike(bike), height = 300, width = 100)
+        self.checkout = Button(self, text = "Checkout", command = lambda: self.checkoutbike(bike), height = 100, width = 100)
         self.checkout.pack()
         self.bike1.destroy()
         self.bike2.destroy()
@@ -22,16 +22,19 @@ class Application(Frame):
     def checkoutbike(self, bike):
         allowed = False
         print("Checking out bike", bike)
-        self.entrythingy = Entry()
-        self.entrythingy.pack()
+        # self.entrythingy = Entry()
         self.checkout.destroy()
+        self.RFID = Button(self, text = "Scan Your RFID Tag", height = 100, width = 100)
+        self.RFID.pack()
         # here is the application variable
-        self.contents = StringVar()
+        # self.contents = StringVar()
         # set it to some value
-        self.contents.set("Scan your RFID")
-        print("Scan Your RFID Tag")
-        # tell the entry widget to watch this variable
-        self.entrythingy["textvariable"] = self.contents
+        # self.contents.set("Scan your RFID")
+        # print("Scan Your RFID Tag")
+        # # tell the entry widget to watch this variable
+        # self.entrythingy["textvariable"] = self.contents
+        # self.entrythingy.pack()
+
 
         p = subprocess.Popen('/home/pi/libnfc-1.4.1/examples/nfc-poll', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(15)
@@ -44,9 +47,15 @@ class Application(Frame):
         if code:
             allowed = True
             print("Authenticated")
+            self.RFID.destroy()
+            self.auth = Button(self, text = "Authenticated", height = 100, width = 100)
+            self.auth.pack()
         else:
             print("Not Authenticated")
             allowed = False
+            self.RFID.destroy()
+            self.auth = Button(self, text = "Not Authenticated", height = 100, width = 100)
+            self.auth.pack()
         if allowed:
             GPIO.output(18, True)
             print("Open the Lock")
@@ -57,9 +66,9 @@ class Application(Frame):
             os.execl('RFID.py')
 
     def createWidgets(self):
-        self.bike1 = Button(self, text = "Bike 1", command = lambda: self.bike(1), height = 300, width = 100)
+        self.bike1 = Button(self, text = "Bike 1", command = lambda: self.bike(1), height = 100, width = 100)
         self.bike1.pack({"side": "left"})
-        self.bike2 = Button(self, text = "Bike 2", command = lambda: self.bike(2), height = 300, width = 100)
+        self.bike2 = Button(self, text = "Bike 2", command = lambda: self.bike(2), height = 100, width = 100)
         self.bike2.pack({"side": "left"})
 
     def __init__(self, master=None):
